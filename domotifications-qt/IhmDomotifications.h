ab073eb798fea5ef96c9f3ce3fa150ebf7ae1473
Thierry Vaira <tvaira@free.fr>
2023-03-29 10:47:28 +0200

    Remanie le code

--- domotifications-qt/IhmDomotifications.h
+++ domotifications-qt/IhmDomotifications.h
@@ -1,7 +1,21 @@
 #ifndef IHMDOMOTIFICATIONS_H
 #define IHMDOMOTIFICATIONS_H
 
+/**
+ * @file IhmDomotifications.h
+ *
+ * @brief Déclaration de la classe IhmDomotifications
+ * @author Mathieu MOYAERTS
+ * @version 0.1
+ */
+
 #include <QtWidgets>
+
+/**
+ * @def TITRE_APPLICATION
+ * @brief Le nom de l'appilcation dans la barre de titre
+ */
+#define TITRE_APPLICATION "Domotifications"
 
 QT_BEGIN_NAMESPACE
 namespace Ui
@@ -10,6 +24,10 @@ class IHMDomotifications;
 }
 QT_END_NAMESPACE
 
+/**
+ * @class IHMDomotifications
+ * @brief La fenêtyre principale de l'application Domotifications
+ */
 class IHMDomotifications : public QMainWindow
 {
     Q_OBJECT
@@ -17,6 +35,7 @@ class IHMDomotifications : public QMainWindow
   public:
     IHMDomotifications(QWidget* parent = nullptr);
     ~IHMDomotifications();
+
     void afficherModules();
     void gererEtatModule();
     void gererActivationModule();
@@ -25,6 +44,7 @@ class IHMDomotifications : public QMainWindow
     void parametrer();
 
   private:
-    Ui::IHMDomotifications* ui;
+    Ui::IHMDomotifications* ui; //!< la GUI
 };
+
 #endif // IHMDOMOTIFICATIONS_H
